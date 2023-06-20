ac49e1569485a0621d0062eaaf006774e6183423
Manuel M <mamueluth@gmail.com>
2023-06-20 12:29:26 +0200

    working extraction

--- plot.py
+++ plot.py
@@ -4,15 +4,37 @@ import os
 import pandas as pd
 import sys
 
+out_of_range = 300
+max_threshold = 8.88
+min_threshold = 6.375
+
 def parse_cli_args():
     parser = argparse.ArgumentParser(description='plot collected data')
     parser.add_argument('file', type=str, help='Serial port which is opened.')
-    parser.add_argument('--plot_type', '-t', type=str, choices=['asc', 'ranged', 'trimm', 'both' ], default='', help='How data is plotted.')
+    parser.add_argument('--plot_type', '-t', type=str, choices=['asc_desc', 'asc_desc_base', 'base', 'cut', 'cut_base'], default='', help='How data is plotted.')
 
 
     return parser.parse_args(args=None if sys.argv[1:] else ["--help"])
 
-def remove_out_of_range(file_name):
+def plot_raw(file_name, color):
+    # Read the CSV file into a pandas DataFrame
+    df = pd.read_csv(file_name)
+
+    # Extract the time and value columns from the DataFrame
+    time_ns = df['Time']
+    values = df['Value']
+    # calculate samples per second
+    start = time_ns.head(1).values[0]
+    stop = time_ns.tail(1).values[0]
+    samples_per_second = len(values)/((stop - start) / 1e9)
+    # Convert time to milliseconds for better visualization
+    time_ms = time_ns / 1e6
+
+    plt.plot(time_ms, values, color=color, label='base')
+
+    return samples_per_second
+
+def plot_base(file_name, color):
     # Read the CSV file into a pandas DataFrame
     df = pd.read_csv(file_name)
 
@@ -20,30 +42,29 @@ def remove_out_of_range(file_name):
     time_ns = df['Time']
     values = df['Value']
     #replace to near or to far with nan
-    values[values > 300] = float('NaN')
+    values[values > out_of_range] = float('NaN')
     # calculate samples per second
     start = time_ns.head(1).values[0]
     stop = time_ns.tail(1).values[0]
     samples_per_second = len(values)/((stop - start) / 1e9)
     # shift start everything relative to start:
     time_ns = time_ns - start
-
     # Convert time to milliseconds for better visualization
     time_ms = time_ns / 1e6
 
-    return time_ms, values, samples_per_second
+    plt.plot(time_ms, values, color=color, label='base')
 
-def asc(file_name):
+    return samples_per_second
+
+def plot_asc_desc(file_name, asc_color, desc_color):
         # Read the CSV file into a pandas DataFrame
     df = pd.read_csv(file_name)
 
     start = df['Time'].head(1).values[0]
+    stop = df['Time'].tail(1).values[0]
+    samples_per_second = len(df['Value'])/((stop - start) / 1e9)
     # shift start everything relative to start:
     df['Time'] = df['Time'] - start
-
-    # Define min and max thresholds
-    min_threshold = 6.375
-    max_threshold = 8.88
 
     # Initialize empty DataFrames for A and B
     ascending_df = pd.DataFrame(columns=['Time', 'Value'])
@@ -70,19 +91,17 @@ def asc(file_name):
     time_ns_asc = ascending_df['Time']
     values_asc = ascending_df['Value']
     time_ms_asc = time_ns_asc / 1e6
+    plt.scatter(time_ms_asc, values_asc, color=asc_color, label='ascending values')
     # descending values and time
     time_ns_desc = descending_df['Time']
     values_desc = descending_df['Value']
     time_ms_desc = time_ns_desc / 1e6
-    # Convert time to milliseconds for better visualization
+    plt.scatter(time_ms_desc, values_desc, color=desc_color, label='descending values')
+
+    return samples_per_second
 
 
-    print(values_asc)
-
-    return time_ms_desc, values_desc, samples_per_second
-
-
-def trimm(file_name):
+def plot_cut(file_name, color):
     # Read the CSV file into a pandas DataFrame
     df = pd.read_csv(file_name)
 
@@ -90,8 +109,8 @@ def trimm(file_name):
     time_ns = df['Time']
     values = df['Value']
     #replace to near or to far with nan
-    values[values > 8.9] = float('NaN')
-    values[values < 6.375] = float('NaN')
+    values[values > max_threshold] = float('NaN')
+    values[values < min_threshold] = float('NaN')
     # calculate samples per second
     start = time_ns.head(1).values[0]
     stop = time_ns.tail(1).values[0]
@@ -102,7 +121,9 @@ def trimm(file_name):
     # Convert time to milliseconds for better visualization
     time_ms = time_ns / 1e6
 
-    return time_ms, values, samples_per_second
+    plt.plot(time_ms, values, color=color, label='cut')
+
+    return samples_per_second
 
 def plot(values, time_ns, color):
     # Create the plot
@@ -115,22 +136,21 @@ if __name__=="__main__":
     plot_type = args.plot_type
 
     samples_per_second = 0.0
-    if plot_type == "trimm":
-        time_ms, values, samples_per_second = trimm(file_name)
-        plt.plot(time_ms, values)
-    elif plot_type == "both":
-        time_ms, values, samples_per_second = trimm(file_name)
-        plt.plot(time_ms, values, color='green', label='trimmed')
-        time_ms, values, _ = remove_out_of_range(file_name)
-        plt.plot(time_ms, values, color='red', label='out_of_range_removed')
-    elif plot_type == "asc":
-        time_ms, values, _ = remove_out_of_range(file_name)
-        plt.plot(time_ms, values, color='green', label='out_of_range_removed')
-        time_ms, values, samples_per_second = asc(file_name)
-        plt.scatter(time_ms, values, color='red', label='out_of_range_removed')
+    if plot_type == "asc_desc":
+        samples_per_second = plot_asc_desc(file_name, 'green', 'red')
+    elif plot_type == "asc_desc_base":
+        samples_per_second = plot_base(file_name, 'blue')
+        plot_asc_desc(file_name, 'green', 'red')
+    elif plot_type == "base":
+        samples_per_second = plot_base(file_name, 'blue')
+    elif plot_type == "cut":
+        samples_per_second = plot_cut(file_name, 'blue')
+    elif plot_type == "cut_base":
+        samples_per_second = plot_base(file_name, 'blue')
+        plot_cut(file_name, 'red')
     else :
-        time_ms, values, samples_per_second = remove_out_of_range(file_name)
-        plt.plot(time_ms, values)
+        print("Plotting raw data")
+        samples_per_second = plot_raw(file_name, 'blue')
 
     plt.xlabel('Time (ms)')
     plt.ylabel('Value')
