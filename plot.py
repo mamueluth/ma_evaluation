23422f9c096544cdd9cb36d69ef85c4ccc87674f
Manuel M <mamueluth@gmail.com>
2023-06-20 17:09:03 +0200

    cleanup plot

--- plot.py
+++ plot.py
@@ -11,64 +11,40 @@ min_threshold = 6.375
 def parse_cli_args():
     parser = argparse.ArgumentParser(description='plot collected data')
     parser.add_argument('file', type=str, help='Serial port which is opened.')
-    parser.add_argument('--plot_type', '-t', type=str, choices=['asc_desc', 'asc_desc_base', 'base', 'cut', 'cut_base'], default='', help='How data is plotted.')
+    parser.add_argument('--plot_type', '-t', type=str, choices=['asc_desc', 'asc_desc_base', 'base', 'cut', 'cut_base', 'raw', 'raw_shifted'], default='', help='How data is plotted.')
 
 
     return parser.parse_args(args=None if sys.argv[1:] else ["--help"])
 
-def plot_raw(file_name, color):
+def read_values(file_name):
     # Read the CSV file into a pandas DataFrame
-    df = pd.read_csv(file_name)
+    return pd.read_csv(file_name)
 
-    # Extract the time and value columns from the DataFrame
-    time_ns = df['Time']
-    values = df['Value']
-    # calculate samples per second
-    start = time_ns.head(1).values[0]
-    stop = time_ns.tail(1).values[0]
-    samples_per_second = len(values)/((stop - start) / 1e9)
-    # Convert time to milliseconds for better visualization
-    time_ms = time_ns / 1e6
-
-    plt.plot(time_ms, values, color=color, label='base')
-
-    return samples_per_second
-
-def plot_base(file_name, color):
-    # Read the CSV file into a pandas DataFrame
-    df = pd.read_csv(file_name)
-
-    # Extract the time and value columns from the DataFrame
-    time_ns = df['Time']
-    values = df['Value']
-    #replace to near or to far with nan
-    values[values > out_of_range] = float('NaN')
-    # calculate samples per second
-    start = time_ns.head(1).values[0]
-    stop = time_ns.tail(1).values[0]
-    samples_per_second = len(values)/((stop - start) / 1e9)
-    # shift start everything relative to start:
-    time_ns = time_ns - start
-    # Convert time to milliseconds for better visualization
-    time_ms = time_ns / 1e6
-
-    plt.plot(time_ms, values, color=color, label='base')
-
-    return samples_per_second
-
-def plot_asc_desc(file_name, asc_color, desc_color):
-        # Read the CSV file into a pandas DataFrame
-    df = pd.read_csv(file_name)
-
+def calculate_samples_per_second(df):
     start = df['Time'].head(1).values[0]
     stop = df['Time'].tail(1).values[0]
     samples_per_second = len(df['Value'])/((stop - start) / 1e9)
-    # shift start everything relative to start:
-    df['Time'] = df['Time'] - start
+    return samples_per_second
 
+def shift_to_zero(df):
+    start = df['Time'].head(1).values[0]
+    # shift start everything relative to start:
+    return df['Time'] - start
+
+def filter_values_out_of_range(values):
+    #replace to near or to far with nan
+    values[values > out_of_range] = float('NaN')
+    return values
+
+def convert_to_ms(time):
+    return time / 1e6
+
+def get_asc_desc_frames(df):
     # Initialize empty DataFrames for A and B
     ascending_df = pd.DataFrame(columns=['Time', 'Value'])
+    ascending_frames = []
     descending_df = pd.DataFrame(columns=['Time', 'Value'])
+    descending_frames = []
 
     # Initialize condition variable
     ascending = True
@@ -80,54 +56,61 @@ def plot_asc_desc(file_name, asc_color, desc_color):
 
         if value < min_threshold:
             ascending = True
+            if not descending_df.empty:
+                descending_frames.append(descending_df)
+                descending_df = pd.DataFrame(columns=['Time', 'Value'])
         elif min_threshold <= value <= max_threshold and ascending:
             ascending_df = pd.concat([ascending_df, row.to_frame().T])
         elif value > max_threshold:
             ascending = False
+            if not ascending_df.empty:
+                ascending_frames.append(ascending_df)
+                ascending_df = pd.DataFrame(columns=['Time', 'Value'])
         elif min_threshold <= value <= max_threshold:
             descending_df = pd.concat([descending_df, row.to_frame().T])
 
+    return ascending_frames, descending_frames
+
+def plot_raw(time, values, color):
+    plt.plot(time_ms, values, color=color, label='base')
+
+def plot_raw_shifted(time, values, color):
+    time_ms = convert_to_ms(time)
+
+    plt.plot(time_ms, values, color=color, label='base')
+
+
+def plot_base(time, values, color):
+    #replace to near or to far with nan
+    values[values > out_of_range] = float('NaN')
+    # Convert time to milliseconds for better visualization
+    time_ms = convert_to_ms(time)
+
+    plt.plot(time_ms, values, color=color, label='base')
+
+
+def plot_asc_desc(ascending_frames, descending_frames, asc_color, desc_color):
     # ascending values and time
-    time_ns_asc = ascending_df['Time']
-    values_asc = ascending_df['Value']
-    time_ms_asc = time_ns_asc / 1e6
-    plt.scatter(time_ms_asc, values_asc, color=asc_color, label='ascending values')
+    for asc_df in ascending_frames:
+        time_ns_asc = asc_df[['Time']]
+        values_asc = asc_df[['Value']]
+        time_ms_asc = convert_to_ms(time_ns_asc)
+        plt.scatter(time_ms_asc, values_asc, color=asc_color, label='ascending values')
     # descending values and time
-    time_ns_desc = descending_df['Time']
-    values_desc = descending_df['Value']
-    time_ms_desc = time_ns_desc / 1e6
-    plt.scatter(time_ms_desc, values_desc, color=desc_color, label='descending values')
+    for desc_df in descending_frames:
+        time_ns_desc = desc_df[['Time']]
+        values_desc = desc_df[['Value']]
+        time_ms_desc = convert_to_ms(time_ns_desc)
+        plt.scatter(time_ms_desc, values_desc, color=desc_color, label='descending values')
 
-    return samples_per_second
-
-
-def plot_cut(file_name, color):
-    # Read the CSV file into a pandas DataFrame
-    df = pd.read_csv(file_name)
-
-    # Extract the time and value columns from the DataFrame
-    time_ns = df['Time']
-    values = df['Value']
+def plot_cut(time, values, color):
     #replace to near or to far with nan
     values[values > max_threshold] = float('NaN')
     values[values < min_threshold] = float('NaN')
-    # calculate samples per second
-    start = time_ns.head(1).values[0]
-    stop = time_ns.tail(1).values[0]
-    samples_per_second = len(values)/((stop - start) / 1e9)
-    # shift start everything relative to start:
-    time_ns = time_ns - start
-
     # Convert time to milliseconds for better visualization
-    time_ms = time_ns / 1e6
+    time_ms = convert_to_ms(time)
 
     plt.plot(time_ms, values, color=color, label='cut')
-
-    return samples_per_second
-
-def plot(values, time_ns, color):
-    # Create the plot
-    plt.plot(time_ms, values)
 
 
 if __name__=="__main__":
@@ -135,22 +118,27 @@ if __name__=="__main__":
     file_name = args.file
     plot_type = args.plot_type
 
-    samples_per_second = 0.0
+    df = read_values(file_name)
+    samples_per_second = calculate_samples_per_second(df)
     if plot_type == "asc_desc":
-        samples_per_second = plot_asc_desc(file_name, 'green', 'red')
+        ascending_frames, descending_frames = get_asc_desc_frames(df)
+        plot_asc_desc(ascending_frames, descending_frames, 'green', 'red')
     elif plot_type == "asc_desc_base":
-        samples_per_second = plot_base(file_name, 'blue')
-        plot_asc_desc(file_name, 'green', 'red')
+        plot_base(df['Time'], df['Value'], 'blue')
+        ascending_frames, descending_frames = get_asc_desc_frames(df)
+        plot_asc_desc(ascending_frames, descending_frames, 'green', 'red')
     elif plot_type == "base":
-        samples_per_second = plot_base(file_name, 'blue')
+        plot_base(df['Time'], df['Value'], 'blue')
     elif plot_type == "cut":
-        samples_per_second = plot_cut(file_name, 'blue')
+        plot_cut(df['Time'], df['Value'], 'blue')
     elif plot_type == "cut_base":
-        samples_per_second = plot_base(file_name, 'blue')
-        plot_cut(file_name, 'red')
-    else :
+        plot_base(df['Time'], df['Value'], 'blue')
+        plot_cut(df['Time'], df['Value'], 'red')
+    elif plot_type == 'raw_shifted' :
+        plot_raw_shifted(df['Time'], df['Value'], 'blue')
+    else:
         print("Plotting raw data")
-        samples_per_second = plot_raw(file_name, 'blue')
+        plot_raw_shifted(df['Time'], df['Value'], 'blue')
 
     plt.xlabel('Time (ms)')
     plt.ylabel('Value')
